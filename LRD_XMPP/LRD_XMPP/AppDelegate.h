25b4a8201f8ff33a9b084b51d7cb10ab75849b89
lanou3g <953733823@qq.com>
2016-02-29 10:13:01 +0800

    测试

---
+++ LRD_XMPP/LRD_XMPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRD_XMPP
+//
+//  Created by lanou3g on 16/2/25.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
